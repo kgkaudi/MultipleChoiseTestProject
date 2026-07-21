9ca115dc52822f41b2fbca37796bd08ad514d894
Kostas <konstantinos.gkaris@gmail.com>
2026-07-21 10:18:13 +0200

    Cleanup user repository

--- backend/repositories/userRepository.js
+++ backend/repositories/userRepository.js
@@ -1,32 +1,26 @@
 const User = require("../models/User");
 
 exports.create = (data) => {
-  const User = require("../models/User");
   return User.create(data);
 };
 
 exports.findAll = () => {
-  const User = require("../models/User");
   return User.find();
 };
 
 exports.findById = (id) => {
-  const User = require("../models/User");
   return User.findById(id);
 };
 
 exports.updateById = (id, data) => {
-  const User = require("../models/User");
   return User.findByIdAndUpdate(id, data, { new: true });
 };
 
 exports.deleteById = (id) => {
-  const User = require("../models/User");
   return User.findByIdAndDelete(id);
 };
 
 exports.updateMany = (filter, data) => {
-  const User = require("../models/User");
   return User.updateMany(filter, data);
 };
 
